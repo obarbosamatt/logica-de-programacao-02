fbfd40a7f1d49e8b17e33d4634801062d366588a
Matheus Barbosa <matheus.maranata@hotmail.com>
2024-04-16 12:08:14 -0300

    Aula 03

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = gerarNumeroAleatorio();  //variavel sempre atribuida no inicio
+let tentativas = 1;
 
 // Podemos alterar os textos do HTML através do JavaScript
 // Para isso, criamos uma variável = document.querySelector (dessa forma identificamos qual atributo do HTML queremos modifgicar)
@@ -18,17 +19,52 @@ function exibirTextoNaTela(tag, texto) {
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela("h1", "Jogo do numero secreto");
-exibirTextoNaTela("p", "Escolha um numero entre 1 e 10");
+function exibirMensagemInicial() {
+    exibirTextoNaTela("h1", "Jogo do numero secreto");
+    exibirTextoNaTela("p", "Escolha um numero entre 1 e 10");
+}
+
+exibirMensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector("input").value;
-    console.log(chute == numeroSecreto);  //booleano - verdadeiro ou falso
+    // console.log(chute == numeroSecreto);  //booleano - verdadeiro ou falso
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela("h1", "Acertou!");
+        let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+        let mensagemTentativas = `Voce descobriu o numero secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela("p", mensagemTentativas);
+        document.getElementById("reiniciar").removeAttribute("disabled");
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela("p", "O numero correto eh menor");
+        } else {
+            exibirTextoNaTela("p", "O numero secreto eh maior");
+        }
+        tentativas++;
+        limparCampo();
+    }
+
 }
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1); //return irá retornar o valor para ser salva na variavel descrita na linha 1
 }
+
+function limparCampo() {
+    chute = document.querySelector("input");
+        chute.value = " ";
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById("reiniciar").setAttribute("disabled", true);
+}
+
 
 
 ///////// PRATICA AULA 02 ////////
